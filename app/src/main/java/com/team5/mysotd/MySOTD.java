079ce774289b2db06403618c114b5b7030f7ce3a
mira <mirasaraswati.jtk12@gmail.com>
2015-06-29 15:12:03 +0700

    Initial commit

---
+++ app/src/main/java/com/team5/mysotd/MySOTD.java
@@ -0,0 +1,7 @@
+package com.team5.mysotd;
+
+/**
+ * Created by Mira on 6/29/2015.
+ */
+public class MySOTD {
+}
---
+++ app/src/main/java/com/team5/mysotd/ShaveDen.java
@@ -0,0 +1,21 @@
+package com.team5.mysotd;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Mira on 6/29/2015.
+ */
+public class ShaveDen extends Fragment {
+    View rootview;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        rootview = inflater.inflate(R.layout.fragment_shave_den, container, false);
+        return rootview;
+    }
+}
